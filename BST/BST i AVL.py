aae16e4d7af14a98a735ab83a86c59ac398b1c9e
Smixie <69311315+Smixie@users.noreply.github.com>
2022-04-04 16:37:46 +0200

    Update BST i AVL.py

--- BST/BST i AVL.py
+++ BST/BST i AVL.py
@@ -18,8 +18,6 @@ class AVLNode():
 
 
 class AVL():
-    post_order_tab = []
-
     def insert(self, root, key):
         if not root:
             return AVLNode(key)
@@ -29,33 +27,33 @@ class AVL():
             root.right_child = self.insert(root.right_child, key)
 
         root.height=1+max(self.heightGet(root.left_child), self.heightGet(root.right_child))
-        #balance factor
+        # balance factor
         balance=self.balance(root)
-        if balance > 1 and key < root.left_child.key: #ll
+        if balance > 1 and key < root.left_child.key:  # ll
             return self.rightR(root)
-        if balance < -1 and key > root.right_child.key: #rr
+        if balance < -1 and key > root.right_child.key:  # rr
             return self.leftR(root)
-        if balance > 1 and key > root.left_child.key: #lr
-            root.left_child=self.leftR(root.left_child)
+        if balance > 1 and key > root.left_child.key:  # lr
+            root.left_child = self.leftR(root.left_child)
             return self.rightR(root)
-        if balance < -1 and key < root.right_child.key: #rl
-            root.right_child=self.rightR(root.right_child)
+        if balance < -1 and key < root.right_child.key:  # rl
+            root.right_child = self.rightR(root.right_child)
             return self.leftR(root)
         return root
 
-    def leftR(self,l): #left Rotate
-        a=l.right_child
-        b=a.left_child
+    def leftR(self, l): # left Rotate
+        a = l.right_child
+        b = a.left_child
 
-        a.left_child=l
-        l.right_child=b
+        a.left_child = l
+        l.right_child = b
 
         l.height = 1 + max(self.heightGet(l.left_child), self.heightGet(l.right_child))
         a.height = 1 + max(self.heightGet(a.left_child), self.heightGet(a.right_child))
 
-        return a #new root
+        return a # new root
 
-    def rightR(self, l): #right rotate
+    def rightR(self, l): # right rotate
         a = l.left_child
         b = a.right_child
         a.right_child = l
@@ -100,7 +98,7 @@ class AVL():
 
         root.height = 1 + max(self.heightGet(root.left_child), self.heightGet(root.right_child))
 
-        balance = self.balance(root) #balancing tree
+        balance = self.balance(root)  # balancing tree
         if balance > 1 and self.balance(root.left_child) >= 0:  # ll
             return self.rightR(root)
         if balance < -1 and self.balance(root.right_child) <= 0:   # rr
@@ -118,14 +116,13 @@ class AVL():
             return root
         return self.min_val(root.left_child)
 
-    def insertion_binary(self, t):
-        if not t:
+    def insertion_binary(self, tab):
+        if not tab:
             return
-        median = len(t) // 2
-        root = AVLNode(t[median])
-        print(t[median])
-        root.left_child = self.insertion_binary(t[:median])  # chyba right_child
-        root.right_child = self.insertion_binary(t[median + 1:])  # chyba left_child
+        median = len(tab) // 2
+        root = AVLNode(tab[median])
+        root.right_child = self.insertion_binary(tab[:median])  # chyba right_child
+        root.left_child = self.insertion_binary(tab[median + 1:])  # chyba left_child
         return root
 
 
@@ -241,153 +238,143 @@ def shell_sort(t, n):
 post_order_tab = []
 tree = AVL()
 while True:
-    print("Menu:\n1 - Dane wejściowe\n2 - Testy\n3 - Zakończ działanie programu")
+    print("Menu:\n1 - BST\n2 - AVL\n3 - Testy\n4 - Zakończ działanie programu")
     x = input()
     if x == "1":
-        while True:
-            root = None
-            print("1 - BST\n2 - AVL\n3 - Powrót")
-            drzewo = input()
-            if drzewo.isnumeric():
-                drzewo = int(drzewo)
-                if drzewo == 1:
-                    print("Ile chcesz podać liczb?")
-                    ile_liczb = input()
-                    if ile_liczb.isnumeric():
-                        ile_liczb = int(ile_liczb)
-                        ile = 0
-                        while ile < ile_liczb:
-                            try:
-                                number = int(input())
-                                if type(number) == int:
-                                    root = insert(root, number)
-                                    ile += 1
-                            except:
-                                print("To nie jest liczba spróbuje jeszcze raz!")
-                        while True:
-                            print(
-                                "Menu procedur:\n1 - Wyszukiwanie min i max\n2 - Usunięcie elementu\n3 - Wypisanie in-order"
-                                "\n4 - Wypisanie pre-order\n5 - Usunięcie całego drzewa(post-order)\n6 - Wypisanie pre-order podrzewa"
-                                "\n7 - Równoważenie drzewa\n8 - Powrót do menu głównego")
-                            #   try:
-                            chosen = int(input())
-                            if type(chosen) == int:
-                                if chosen == 1:
-                                    print("Największa wartość : ")
-                                    find_max(root)
-                                    print("Najmniejsza wartość : ")
-                                    find_min(root)
-                                    print("\n")
-                                if chosen == 2:
-                                    print("Podaj ile wartości chcesz usunąć: ")
-                                    value = int(input())
-                                    if type(value) == int:
-                                        for v in range(value):
-                                            value_td = int(input())
-                                            root = delete(root, value_td)
-                                    print("\n")
-                                if chosen == 3:
-                                    print("In-order")
-                                    in_order(root)
-                                    print("\n")
-                                if chosen == 4:
-                                    print("Pre-order")
-                                    pre_order(root)
-                                    print("\n")
-                                if chosen == 5:
-                                    print("Delete post-order")
-                                    post_order(root, post_order_tab)
-                                    for x in post_order_tab:
-                                        root = delete(root, x)
-                                    print("\n")
-                                if chosen == 6:
-                                    print("Podaj klucz do podrzewa: ")
-                                    key = int(input())
-                                    if type(key) == int:
-                                        print("Find a key")
-                                        find_and_print(root, key)
-                                        print("\n")
-                                if chosen == 7:
-                                    print("Maintenance in progress")  #
-                                if chosen == 8:
-                                    break
-                        #  except:
-                        #    print("To chyba nie jest poprawna wartość! Spróbuj ponownie.")
-                    else:
-                        print("To nie jest liczba podaj poprawną wartość!")
-                if drzewo == 2:
-                    print("Ile chcesz podać liczb?")
-                    ile_liczb = input()
-                    if ile_liczb.isnumeric():
-                        ile_liczb = int(ile_liczb)
-                        ile = 0
-                        while ile < ile_liczb:
-                            # try:
-                            number = int(input())
-                            if type(number) == int:
-                                root = tree.insert(root, number)
-                                ile += 1
-                        # except:
-                        #   print("To nie jest liczba spróbuj jeszcze raz!")
-                        while True:
-                            print(
-                                "Menu procedur:\n1 - Wyszukiwanie min i max\n2 - Usunięcie elementu\n3 - Wypisanie in-order"
-                                "\n4 - Wypisanie pre-order\n5 - Usunięcie całego drzewa(post-order)\n6 - Wypisanie pre-order podrzewa"
-                                "\n7 - Równoważenie drzewa\n8 - Powrót do menu głównego")
-                            # try:
-                            chosen = int(input())
-                            if type(chosen) == int:
-                                if chosen == 1:
-                                    print("Największa wartość : ")
-                                    find_max(root)
-                                    print("Najmniejsza wartość : ")
-                                    find_min(root)
-                                    print("\n")
-                                if chosen == 2:
-                                    print("Podaj ile wartości chcesz usunąć: ")
-                                    value = int(input())
-                                    if type(value) == int:
-                                        for v in range(value):
-                                            value_td = int(input())
-                                            if type(value_td) == int:
-                                                root = tree.delete(root, value_td)
-                                    print("\n")
-                                if chosen == 3:
-                                    print("In-order")
-                                    in_order(root)
-                                    print("\n")
-                                if chosen == 4:
-                                    print("Pre-order")
-                                    pre_order(root)
-                                    print("\n")
-                                if chosen == 5:
-                                    print("Delete post-order")
-                                    post_order(root, post_order_tab)
-                                    for x in post_order_tab:
-                                        root = delete(root, x)
-                                    print("\n")
-                                if chosen == 6:
-                                    print("Podaj klucz do podrzewa: ")
-                                    key = int(input())
-                                    if type(key) == int:
-                                        print("Find a key")
-                                        find_and_print(root, key)
-                                        print("\n")
-                                if chosen == 7:
-                                    print("Nie wiem o co cmon")  #
-                                if chosen == 8:
-                                    break
-                            # except:
-                            #  print("To chyba nie jest poprawna wartość! Spróbuj ponownie.")
-                    else:
-                        print("To nie jest liczba podaj poprawną wartość!")
-                if drzewo == 3:
-                    break
-            else:
-                print("Podaj poprawną wartość!")
-    elif x == "2":
-        print(x)
-    elif x == "3":
+        root = None
+        print("Ile chcesz podać liczb?")
+        ile_liczb = input()
+        if ile_liczb.isnumeric():
+            ile_liczb = int(ile_liczb)
+            ile = 0
+            while ile < ile_liczb:
+                try:
+                    number = int(input())
+                    if type(number) == int:
+                        root = insert(root, number)
+                        ile += 1
+                except:
+                    print("To nie jest liczba spróbuje jeszcze raz!")
+            while True:
+                print(
+                    "Menu procedur:\n1 - Wyszukiwanie min i max\n2 - Usunięcie elementu\n3 - Wypisanie in-order"
+                    "\n4 - Wypisanie pre-order\n5 - Usunięcie całego drzewa(post-order)\n6 - Wypisanie pre-order podrzewa"
+                    "\n7 - Równoważenie drzewa\n8 - Powrót do menu głównego")
+                #   try:
+                chosen = int(input())
+                if type(chosen) == int:
+                    if chosen == 1:
+                        print("Największa wartość : ")
+                        find_max(root)
+                        print("Najmniejsza wartość : ")
+                        find_min(root)
+                        print("\n")
+                    if chosen == 2:
+                        print("Podaj ile wartości chcesz usunąć: ")
+                        value = int(input())
+                        if type(value) == int:
+                            for v in range(value):
+                                value_td = int(input())
+                                root = delete(root, value_td)
+                        print("\n")
+                    if chosen == 3:
+                        print("In-order")
+                        in_order(root)
+                        print("\n")
+                    if chosen == 4:
+                        print("Pre-order")
+                        pre_order(root)
+                        print("\n")
+                    if chosen == 5:
+                        print("Delete post-order")
+                        post_order(root, post_order_tab)
+                        for x in post_order_tab:
+                            root = delete(root, x)
+                        print("\n")
+                    if chosen == 6:
+                        print("Podaj klucz do podrzewa: ")
+                        key = int(input())
+                        if type(key) == int:
+                            print("Find a key")
+                            find_and_print(root, key)
+                            print("\n")
+                    if chosen == 7:
+                        print("Maintenance in progress")  #
+                    if chosen == 8:
+                        break
+            #  except:
+            #    print("To chyba nie jest poprawna wartość! Spróbuj ponownie.")
+        else:
+            print("To nie jest liczba podaj poprawną wartość!")
+    if x == "2":
+        print("Ile chcesz podać liczb?")
+        ile_liczb = input()
+        if ile_liczb.isnumeric():
+            ile_liczb = int(ile_liczb)
+            ile = 0
+            data = []
+            while ile < ile_liczb:
+                # try:
+                number = int(input())
+                if type(number) == int:
+                    data.append(number)
+                    ile += 1
+            root = tree.insertion_binary(data)
+            # except:
+            #   print("To nie jest liczba spróbuj jeszcze raz!")
+            while True:
+                print(
+                    "Menu procedur:\n1 - Wyszukiwanie min i max\n2 - Usunięcie elementu\n3 - Wypisanie in-order"
+                    "\n4 - Wypisanie pre-order\n5 - Usunięcie całego drzewa(post-order)\n6 - Wypisanie pre-order podrzewa"
+                    "\n7 - Powrót do menu głównego")
+                # try:
+                chosen = int(input())
+                if type(chosen) == int:
+                    if chosen == 1:
+                        print("Największa wartość : ")
+                        find_max(root)
+                        print("\nNajmniejsza wartość : ")
+                        find_min(root)
+                        print("\n")
+                    if chosen == 2:
+                        print("Podaj ile wartości chcesz usunąć: ")
+                        value = int(input())
+                        if type(value) == int:
+                            for v in range(value):
+                                value_td = int(input())
+                                if type(value_td) == int:
+                                    root = tree.delete(root, value_td)
+                        print("\n")
+                    if chosen == 3:
+                        print("In-order")
+                        in_order(root)
+                        print("\n")
+                    if chosen == 4:
+                        print("Pre-order")
+                        pre_order(root)
+                        print("\n")
+                    if chosen == 5:
+                        print("Delete post-order")
+                        post_order(root, post_order_tab)
+                        for x in post_order_tab:
+                            root = tree.delete(root, x)
+                        print("\n")
+                    if chosen == 6:
+                        print("Podaj klucz do podrzewa: ")
+                        key = int(input())
+                        if type(key) == int:
+                            print("Ścieżka od klucza")
+                            find_and_print(root, key)
+                            print("\n")
+                    if chosen == 7:
+                        break
+                # except:
+                #  print("To chyba nie jest poprawna wartość! Spróbuj ponownie.")
+        else:
+            print("To nie jest liczba podaj poprawną wartość!")
+    if x == "3":
+        print("Maintenance in progress")
+    if x == "4":
         sys.exit()
     else:
         print("Podaj poprawną wartość")
